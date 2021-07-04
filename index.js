e1538b241841a21ad4063addb735cdbce7ca7f8f
Olivia Park <oliviaypark113@gmail.com>
2021-07-03 21:49:33 -0400

    updated deploying

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ app.use("/api/posts", postRoute);
 
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+  app.use('/static', express.static(path.join(__dirname, 'client/build')));
 }
 
 app.listen(PORT, () => {
